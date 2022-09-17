8543b294282c2ba5c02bb44d380b987a53b05914
michel-de-almeida <micheldealmeida0@gmail.com>
2022-09-17 21:51:41 +0200

    started

--- packages/backend/dist/server.js
+++ packages/backend/dist/server.js
@@ -3,48 +3,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const apollo_server_express_1 = require("apollo-server-express");
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const db_1 = require("./config/db");
 const errorMiddleware_1 = __importDefault(require("./middleware/errorMiddleware"));
-const noteRoute_1 = __importDefault(require("./routes/noteRoute"));
-const ticketRoutes_1 = __importDefault(require("./routes/ticketRoutes"));
-const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
-const apollo_server_express_1 = require("apollo-server-express");
-dotenv_1.default.config();
-const port = process.env.PORT || 5000;
-(0, db_1.connectDB)();
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-const typeDefs = (0, apollo_server_express_1.gql) `
-    type Query {
-        hello: String
+const helloResolver_1 = require("./resolvers/helloResolver");
+const type_graphql_1 = require("type-graphql");
+;
+(async () => {
+    dotenv_1.default.config();
+    const port = process.env.PORT || 5000;
+    (0, db_1.connectDB)();
+    const app = (0, express_1.default)();
+    const server = new apollo_server_express_1.ApolloServer({ schema: await (0, type_graphql_1.buildSchema)({ resolvers: [helloResolver_1.HelloResolver] }) });
+    server.applyMiddleware({ app });
+    app.use(errorMiddleware_1.default);
+    if (process.env.NODE_ENV === 'prod') {
+        app.use(express_1.default.static(path_1.default.join(__dirname, '../../frontend/build')));
+        app.get('*', (_req, res) => {
+            res.sendFile(path_1.default.join(__dirname, '../../frontend/build/index.html'));
+        });
     }
-`;
-const resolvers = {
-    Query: { hello: () => 'bruh' },
-};
-const server = new apollo_server_express_1.ApolloServer({ typeDefs, resolvers, csrfPrevention: true, cache: 'bounded' });
-server.start().then(() => server.applyMiddleware({ app }));
-app.use('/api/users', userRoutes_1.default);
-app.use('/api/tickets', ticketRoutes_1.default);
-app.use('/api/notes', noteRoute_1.default);
-app.use(errorMiddleware_1.default);
-if (process.env.NODE_ENV === 'prod') {
-    app.use(express_1.default.static(path_1.default.join(__dirname, '../../frontend/build')));
-    app.get('*', (_req, res) => {
-        res.sendFile(path_1.default.join(__dirname, '../../frontend/build/index.html'));
+    else {
+        app.get('/', (_req, res) => {
+            res.status(200).json({ message: 'Welcome to support desk API' });
+        });
+    }
+    app.listen(port, () => {
+        console.log(`Express listening on port ${port}`);
     });
-}
-else {
-    app.get('/', (_req, res) => {
-        res.status(200).json({ message: 'Welcome to support desk API' });
-    });
-}
-app.listen(port, () => {
-    console.log(`Express listening on port ${port}`);
-    console.log(`Apollo server on http://localhost:${port}${server.graphqlPath}`);
-});
+})();
 //# sourceMappingURL=server.js.map
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server-express'
 import dotenv from 'dotenv'
 import express from 'express'
 import path from 'path'
 import { connectDB } from './config/db'
 import errorHandler from './middleware/errorMiddleware'
-import noteRouter from './routes/noteRoute'
-import ticketRouter from './routes/ticketRoutes'
-import userRouter from './routes/userRoutes'
+import { HelloResolver } from './resolvers/helloResolver'
+import { buildSchema } from 'type-graphql'
 
 declare global {
     namespace Express {
@@ -15,32 +16,37 @@ declare global {
     }
 }
 
-dotenv.config()
-const port = process.env.PORT || 5000
-connectDB()
+;(async () => {
+    dotenv.config()
+    const port = process.env.PORT || 5000
+    connectDB()
+    const app = express()
 
-const app = express()
+    const server = new ApolloServer({ schema: await buildSchema({ resolvers: [HelloResolver] }) })
+    await server.start()
+    server.applyMiddleware({ app })
+    app.use(errorHandler)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+    if (process.env.NODE_ENV === 'prod') {
+        app.use(express.static(path.join(__dirname, '../../frontend/build')))
 
-app.use('/api/users', userRouter)
-app.use('/api/tickets', ticketRouter)
-app.use('/api/notes', noteRouter)
-app.use(errorHandler)
+        app.get('*', (_req, res) => {
+            res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
+        })
+    } else {
+        app.get('/', (_req, res) => {
+            res.status(200).json({ message: 'Welcome to support desk API' })
+        })
+    }
 
-if (process.env.NODE_ENV === 'prod') {
-    app.use(express.static(path.join(__dirname, '../../frontend/build')))
-
-    app.get('*', (_req, res) => {
-        res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
+    app.listen(port, () => {
+        console.log(`Express listening on port ${port}`)
     })
-} else {
-    app.get('/', (_req, res) => {
-        res.status(200).json({ message: 'Welcome to support desk API' })
-    })
-}
+})()
 
-app.listen(port, () => {
-    console.log(`Express listening on port ${port}`)
-})
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// app.use('/api/users', userRouter)
+// app.use('/api/tickets', ticketRouter)
+// app.use('/api/notes', noteRouter)
