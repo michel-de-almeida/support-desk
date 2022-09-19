ff0e3a280574486c0a884d338be8ca045d5a1281
michel-de-almeida <micheldealmeida0@gmail.com>
2022-09-19 15:03:35 +0200

    ticket crud

--- packages/backend/src/resolvers/ticketResolver.ts
+++ packages/backend/src/resolvers/ticketResolver.ts
@@ -1,32 +1,138 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { UserModel } from '../entities/userEntity'
+import { IAppContext } from '../interfaces'
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { Ticket, TicketModel } from '../entities/ticketEntity'
-import { TicketInput } from './types/ticket-IO'
+import {
+    CreateTicketInput,
+    UpdateTicketInput,
+    TicketResponse,
+    TicketsResponse,
+} from './types/ticket-IO'
 
 @Resolver(Ticket)
 export class TicketResolver {
-    @Query(() => Ticket, { nullable: true })
-    ticket(@Arg('id') id: string) {
-        return TicketModel.findById(id)
+    @Query(() => TicketsResponse)
+    async userTickets(@Ctx() { req }: IAppContext): Promise<TicketsResponse> {
+        const tickets = await TicketModel.find({userRef.id = req})
+
+        if (!tickets) {
+            return {
+                success: false,
+                errors: [{ message: 'An error occured while attempting to fetch tickets' }],
+            }
+        }
+        return { success: true, tickets: tickets }
     }
 
-    @Query(() => [Ticket])
-    async tickets(): Promise<Ticket[]> {
-        return await TicketModel.find()
+    @Query(() => TicketsResponse)
+    async tickets(): Promise<TicketsResponse> {
+        const tickets = await TicketModel.find()
+
+        if (!tickets) {
+            return {
+                success: false,
+                errors: [{ message: 'An error occured while attempting to fetch tickets' }],
+            }
+        }
+        return { success: true, tickets: tickets }
     }
 
-    @Mutation(() => TicketModel)
-    async setTicket(@Arg('ticket') options: TicketInput): Promise<Ticket> {
+    @Query(() => TicketResponse, { nullable: true })
+    async getTicket(@Arg('ticketId') id: string): Promise<TicketResponse> {
+        if (!id) {
+            return {
+                success: false,
+                errors: [{ field: 'ticketId', message: 'Please provide a ticket id' }],
+            }
+        }
+        const ticket = await TicketModel.findById(id)
+
+        if (!ticket) {
+            return { success: false, errors: [{ message: 'Ticket not found' }] }
+        }
+
+        return { success: true, ticket: ticket }
+    }
+
+    @Mutation(() => TicketResponse)
+    async setTicket(
+        @Arg('ticket') options: CreateTicketInput,
+        @Ctx() { req }: IAppContext
+    ): Promise<TicketResponse> {
+        if (!options.product) {
+            return { success: false, errors: [{ message: 'Please provide a product' }] }
+        }
+
+        if (!options.description) {
+            return { success: false, errors: [{ message: 'Please provide a description' }] }
+        }
+
+        const user = await UserModel.findById(req.session.userId)
         const ticket: Ticket = {
             ...options,
-            userRef: undefined,
+            userRef: user!,
         }
-        const newTicket = TicketModel.create(ticket)
-        // const recipe = new RecipeModel({
-        //     ...recipeInput,
-        //     author: user._id,
-        // } as Recipe)
+        const newTicket = await TicketModel.create(ticket)
 
-        // await recipe.save()
-        return newTicket
+        if (!newTicket) {
+            return {
+                success: false,
+                errors: [{ message: 'An error occured while attempting to create a ticket' }],
+            }
+        }
+
+        return { success: true, ticket: newTicket }
+    }
+
+    @Mutation(() => TicketResponse)
+    async updateTicket(@Arg('ticket') options: UpdateTicketInput): Promise<TicketResponse> {
+        if (!options.id) {
+            return { success: false, errors: [{ message: 'Please provide the ticketId' }] }
+        }
+
+        if (!options.product) {
+            return { success: false, errors: [{ message: 'Please provide a product' }] }
+        }
+
+        if (!options.description) {
+            return { success: false, errors: [{ message: 'Please provide a description' }] }
+        }
+
+        const ticketExists = await TicketModel.findById(options.id)
+
+        if (!ticketExists) {
+            return { success: false, errors: [{ message: 'Ticket not found' }] }
+        }
+
+        const updatedTicket = await TicketModel.findByIdAndUpdate(options.id, options, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!updatedTicket) {
+            return {
+                success: false,
+                errors: [{ message: 'An error occured while attempting to update the ticket' }],
+            }
+        }
+
+        return { success: true, ticket: updatedTicket }
+    }
+
+    @Mutation(() => TicketResponse, { nullable: true })
+    async deleteTicket(@Arg('ticketId') id: string): Promise<TicketResponse> {
+        if (!id) {
+            return {
+                success: false,
+                errors: [{ field: 'ticketId', message: 'Please provide a ticket id' }],
+            }
+        }
+        const ticket = await TicketModel.findByIdAndDelete(id)
+
+        if (!ticket) {
+            return { success: false, errors: [{ message: 'Ticket not found' }] }
+        }
+
+        return { success: true }
     }
 }
